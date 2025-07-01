4d16ad5f7341a3004bfb10bc1ae71010c346752d
Preveen369 <spreveen123@gmail.com>
2025-07-01 12:58:42 +0530

    Added SQL file, configured the DB connection.

--- src/personalfinancemanager/util/DBUtil.java
+++ src/personalfinancemanager/util/DBUtil.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package personalfinancemanager.util;
 
-/**
- *
- * @author PREVEEN S
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBUtil {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/personal_finance_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("[X] Connection failed: " + e.getMessage());
+            return null;
+        }
+    }
 }
